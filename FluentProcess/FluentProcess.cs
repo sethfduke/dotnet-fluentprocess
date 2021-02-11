dc350c911785e867dfa3b6832f6046ebd0a24b9c
Seth Duke <seth.duke@hyland.com>
2021-02-11 11:34:25 -0500

    feat: initial release

---
+++ FluentProcess/FluentProcess.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security;
+using System.Text;
+
+namespace FluentProcessBuilder
+{
+    public class FluentProcess : Process
+    {
+        public new FluentProcess BeginErrorReadLine()
+        {
+            base.BeginErrorReadLine();
+            return this;
+        }
+        
+        public new FluentProcess BeginOutputReadLine()
+        {
+            base.BeginOutputReadLine();
+            return this;
+        }
+
+        public new FluentProcess WaitForExit()
+        {
+            base.WaitForExit();
+            return this;
+        }
+
+        public new FluentProcess Start()
+        {
+            base.Start();
+            return this;
+        }
+
+        public new FluentProcess OutputDataReceived(DataReceivedEventHandler handler)
+        {
+            base.OutputDataReceived += handler;
+            return this;
+        }
+        
+        public new FluentProcess ErrorDataReceived(DataReceivedEventHandler handler)
+        {
+            base.ErrorDataReceived += handler;
+            return this;
+        }
+        
+        public FluentProcess PriorityClass(ProcessPriorityClass priorityClass = default)
+        {
+            base.PriorityClass = priorityClass == default ? ProcessPriorityClass.Normal : priorityClass;
+            return this;
+        }
+        
+        public FluentProcess ProcessorAffinity(IntPtr ptr)
+        {
+            base.ProcessorAffinity = ptr;
+            return this;
+        }
+        
+        public FluentProcess SynchronizingObject(ISynchronizeInvoke invoke)
+        {
+            base.SynchronizingObject = invoke;
+            return this;
+        }
+        
+        public FluentProcess Site(ISite site)
+        {
+            base.Site = site;
+            return this;
+        }
+        
+        public FluentProcess PriorityBoostEnabled(bool enabled = true)
+        {
+            base.PriorityBoostEnabled = enabled;
+            return this;
+        }
+
+        public FluentProcess EnableRaisingEvents(bool enabled = true)
+        {
+            base.EnableRaisingEvents = enabled;
+            return this;
+        }
+        
+        public FluentProcess MaxWorkingSet(IntPtr ptr)
+        {
+            base.MaxWorkingSet = ptr;
+            return this;
+        }
+        
+        public FluentProcess MinWorkingSet(IntPtr ptr)
+        {
+            base.MinWorkingSet = ptr;
+            return this;
+        }
+        
+        public FluentProcess FileName(string filename)
+        {
+            StartInfo.FileName = filename;
+            return this;
+        }
+        
+        public FluentProcess ArgumentList(params string[] args)
+        {
+            foreach (var s in args)
+            {
+                StartInfo.ArgumentList.Add(s);
+            }
+            return this;
+        }
+        
+        public FluentProcess Arguments(string args)
+        {
+            StartInfo.Arguments = args;
+            return this;
+        }
+        
+        public FluentProcess CreateNoWindow(bool enabled = true)
+        {
+            StartInfo.CreateNoWindow = enabled;
+            return this;
+        }
+        
+        public FluentProcess Domain(string domain)
+        {
+            StartInfo.Domain = domain;
+            return this;
+        }
+        
+        public FluentProcess Environment(params KeyValuePair<string,string>[] vars)
+        {
+            foreach (var v in vars)
+            {
+                StartInfo.Environment.Add(v);
+            }
+            return this;
+        }
+        
+        public FluentProcess EnvironmentVariables(params KeyValuePair<string,string>[] vars)
+        {
+            foreach (var v in vars)
+            {
+                StartInfo.EnvironmentVariables.Add(v.Key, v.Value);   
+            }
+            return this;
+        }
+        
+        public FluentProcess ErrorDialog(bool enabled = true)
+        {
+            StartInfo.ErrorDialog = enabled;
+            return this;
+        }
+        
+        public FluentProcess ErrorDialogHandle(IntPtr ptr)
+        {
+            StartInfo.ErrorDialogParentHandle = ptr;
+            return this;
+        }
+        
+        public FluentProcess LoadUserProfile(bool enabled = true)
+        {
+            StartInfo.LoadUserProfile = enabled;
+            return this;
+        }
+        
+        public FluentProcess Password(SecureString password)
+        {
+            StartInfo.Password = password;
+            return this;
+        }
+        
+        public FluentProcess PasswordInClearText(string password)
+        {
+            StartInfo.PasswordInClearText = password;
+            return this;
+        }
+        
+        public FluentProcess RedirectStandardError(bool enabled = true)
+        {
+            StartInfo.RedirectStandardError = enabled;
+            return this;
+        }
+        
+        public FluentProcess RedirectStandardInput(bool enabled = true)
+        {
+            StartInfo.RedirectStandardInput = enabled;
+            return this;
+        }
+        
+        public FluentProcess RedirectStandardOutput(bool enabled = true)
+        {
+            StartInfo.RedirectStandardOutput = enabled;
+            return this;
+        }
+        
+        public FluentProcess StandardErrorEncoding(Encoding encoding = null)
+        {
+            StartInfo.StandardErrorEncoding = encoding ?? Encoding.Default;
+            return this;
+        }
+        
+        public FluentProcess StandardInputEncoding(Encoding encoding = null)
+        {
+            StartInfo.StandardInputEncoding = encoding ?? Encoding.Default;
+            return this;
+        }
+        
+        public FluentProcess StandardOutputEncoding(Encoding encoding = null)
+        {
+            StartInfo.StandardOutputEncoding = encoding ?? Encoding.Default;
+            return this;
+        }
+        
+        public FluentProcess Username(string username)
+        {
+            StartInfo.UserName = username;
+            return this;
+        }
+        
+        public FluentProcess UseShellExecute(bool enabled = true)
+        {
+            StartInfo.UseShellExecute = enabled;
+            return this;
+        }
+        
+        public FluentProcess Verb(string verb)
+        {
+            StartInfo.Verb = verb;
+            return this;
+        }
+        
+        public FluentProcess WindowStyle(ProcessWindowStyle style = default)
+        {
+            StartInfo.WindowStyle = style == default ? ProcessWindowStyle.Normal : style;
+            return this;
+        }
+        
+        public FluentProcess WorkingDirectory(string directory)
+        {
+            StartInfo.WorkingDirectory = directory;
+            return this;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,56 @@
+﻿# FluentProcess
+
+The FluentProcess library is sub-class of the Systems.Diagnostics.Process class and provides fluent style methods for building and executing a process without the need to separately define the StartInfo property. The construction and execution of a Process can be completed in a single line of code.
+
+## Installation
+
+The library is available via Nuget at https://www.nuget.org/packages/FluentProcess
+
+## Usage
+
+```
+var p = new FluentProcess()
+    .FileName("dotnet")
+    .Arguments("--help")
+    .UseShellExecute(false)
+    .CreateNoWindow()
+    .RedirectStandardError()
+    .RedirectStandardOutput()
+    .ErrorDataReceived((_, e) => Console.WriteLine(e.Data))
+    .OutputDataReceived((_, e) => Console.WriteLine(e.Data))
+    .Start()
+    .BeginOutputReadLine()
+    .WaitForExit();
+```
+
+## Issues
+
+Please submit any issues or feature requests to https://github.com/sethfduke/dotnet-fluentprocess/issues
+
+## Contributing
+
+1. Fork this repository.
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## History
+
+Version 1.0.0 (2021-02-11) - Initial release
+
+## Credits
+
+Seth Duke (@sethfduke)
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2021 Seth Duke
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
